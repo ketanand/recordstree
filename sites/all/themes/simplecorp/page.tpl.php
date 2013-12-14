06e0af7f660717891958eb79ecbff91f50c5e4b8
Saurabh <saurabh@saurabh.(none)>
2013-12-15 04:06:44 +0530

    fb and twitter buttons added

--- sites/all/themes/simplecorp/page.tpl.php
+++ sites/all/themes/simplecorp/page.tpl.php
@@ -1,11 +1,20 @@
 <!-- #page-wrapper -->
+<!-- fb sdk -->
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
 <div id="page-wrapper">
 
     <!-- #page -->
     <div id="page">
         
         <!-- header -->
-        <header role="header" class="container clearfix">
+        <header id="header-wrapper" role="header" class="container clearfix">
         
             <!-- #pre-header -->
             <div id="pre-header" class="clearfix">
@@ -16,17 +25,22 @@
 
                 <?php if (theme_get_setting('social_icons_display','simplecorp')): ?>   
                     <!-- #social-icons -->
+		    <div class="fb-like" data-href="https://www.facebook.com/recordstree" data-layout="button_count" data-action="like" data-show-faces="true" data-share="true"></div>
+		    <div id="twitter-follow" >
+			    <a href="https://twitter.com/RecordsTree" class="twitter-follow-button" data-show-count="false" data-show-screen-name="false">Follow @RecordsTree</a>
+	<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
+		    </div>
                     <div id="social-icons" class="clearfix">
                         <ul id="social-links">
-                            <li class="facebook-link"><a href="https://www.facebook.com/morethan.just.themes" class="facebook" id="social-01" title="Join Us on Facebook!">Facebook</a></li>
-                            <li class="twitter-link"><a href="https://twitter.com/morethanthemes" class="twitter" id="social-02" title="Follow Us on Twitter">Twitter</a></li>
+                            <li class="facebook-link"><a href="https://www.facebook.com/recordstree" class="facebook" id="social-01" title="Join Us on Facebook!">Facebook</a></li>
+                            <li class="twitter-link"><a href="https://twitter.com/recordstree" class="twitter" id="social-02" title="Follow Us on Twitter">Twitter</a></li>
                             <li class="google-link"><a href="#" id="social-03" title="Google" class="google">Google</a></li>
-                            <li class="dribbble-link"><a href="#" id="social-04" title="Dribble" class="dribbble">Dribble</a></li>
+                            <!--li class="dribbble-link"><a href="#" id="social-04" title="Dribble" class="dribbble">Dribble</a></li>
                             <li class="vimeo-link"><a href="#" id="social-05" title="Vimeo" class="vimeo">Vimeo</a></li>
                             <li class="skype-link"><a href="#" id="social-06" title="Skype" class="skype">Skype</a></li>
                             <li class="linkedin-link"><a href="#" id="social-07" title="Linkedin" class="linkedin">Linkedin</a></li>
                             <li class="pinterest-link"><a href="#" id="social-09" title="Pinterest" class="pinterest">Pinterest</a></li>
-                            <li class="rss-link"><a href="#" id="social-08" title="RSS" class="rss">RSS Feeds</a></li>
+                            <li class="rss-link"><a href="#" id="social-08" title="RSS" class="rss">RSS Feeds</a></li-->
                         </ul>
                     </div>
                     <!-- EOF: #social-icons -->
